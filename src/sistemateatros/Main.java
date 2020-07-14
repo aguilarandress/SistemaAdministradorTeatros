6013c8ac80e89291e43a7008315d68b1568617f9
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-07-14 16:26:40 -0600

    Better authentication

--- src/sistemateatros/Main.java
+++ src/sistemateatros/Main.java
@@ -1,19 +1,15 @@
 package sistemateatros;
 
-import org.mindrot.jbcrypt.BCrypt;
 import sistemateatros.authmanager.AuthenticationManager;
 import sistemateatros.controllers.AuthenticationController;
-import sistemateatros.database.DatabaseConnection;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Authentication Setup
+        // AuthenticationSetup
         AuthenticationManager.loadAuthenticationUsers();
-
         // Start application
         AuthenticationController authenticationController = new AuthenticationController();
     }
-
 
 }
