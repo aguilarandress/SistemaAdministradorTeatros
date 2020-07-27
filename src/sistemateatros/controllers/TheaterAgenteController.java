a71b43e0bb5e013be458ef529ab301c306b945ef
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-07-27 14:09:15 -0600

    Dump de base de datos con permisos completos

--- src/sistemateatros/controllers/TheaterAgenteController.java
+++ src/sistemateatros/controllers/TheaterAgenteController.java
@@ -335,7 +335,6 @@ public class TheaterAgenteController {
             if (repetido)
             {
                 agentView.displayMessage("No se agregaron los asientos repetidos",false);
-
             }
 
         }
